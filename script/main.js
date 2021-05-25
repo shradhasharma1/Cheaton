6ce123d14e9b02b0451aaed726c8cfb270e2e57c
shradhasharma1 <71868942+shradhasharma1@users.noreply.github.com>
2021-05-25 16:51:07 +0530

    Update main.js

--- script/main.js
+++ script/main.js
@@ -171,6 +171,8 @@ const animationTimeline = () => {
     .to(".idea-1.4", 0.7, ideaTextTransLeave, "+=1.5")
     .from(".idea-1.5", 0.7, ideaTextTrans)
     .to(".idea-1.5", 0.7, ideaTextTransLeave, "+=1.5")
+    .from(".idea-1.6", 0.7, ideaTextTrans)
+    .to(".idea-1.6", 0.7, ideaTextTransLeave, "+=1.5")
     .from(".idea-2", 0.7, ideaTextTrans)
     .to(".idea-2", 0.7, ideaTextTransLeave, "+=1.5")
     .from(".idea-3", 0.7, ideaTextTrans)
